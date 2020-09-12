0aad824d1a98345bb7d1c27d5d711645b652b0e2
Brandon Phillips <brandon.phillips976@gmail.com>
2020-09-11 18:42:16 -0700

    fixed bug when user click start more than once

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 var getTime = document.querySelector('h2').textContent;
 var currentTime = parseFloat(getTime)
 var timeInterval;
+var hasStarted = false;
 
 function start() {
-    console.log(currentTime, typeof(currentTime))
-    timeInterval = setInterval(function() {
-        currentTime += .01;
-        document.querySelector('h2').textContent = currentTime.toFixed(2);
-    }, 10);
+    if (!hasStarted) {
+        hasStarted = true;
+        console.log(currentTime, typeof(currentTime))
+        timeInterval = setInterval(function() {
+            currentTime += .01;
+            document.querySelector('h2').textContent = currentTime.toFixed(2);
+        }, 10);
+    }
 }
 
 function stop() {
     clearInterval(timeInterval)
+    hasStarted = false;
 };
 
 function reset() {
